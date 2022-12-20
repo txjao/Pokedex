bfc3b1c3f4754b4c52541ca324450f2f8d5846f9
Joao <jaobacanao@gmail.com>
2022-12-20 01:36:04 -0300

    feat: stylizing the card

--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -8,6 +8,11 @@ const Container = styled.div`
     height: 380px;
     width: 250px;
 
+    background-color: #fff;
+
+    border: 8px solid #4B5563;
+    border-radius: 14px;
+
     .stats-box{
         background-color: #E5E7EB;
     
@@ -16,13 +21,60 @@ const Container = styled.div`
 
     .top-info{
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 70%;
+    }
+
+
+    .container-type{
+        background-color: #374151;
+        border-radius: 14px;
+        color: #fff;
+
+        padding: 5px;
+
+        width: 60px;
+        height: 24px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+
+    .type{
+        font-size: 12px;
+    }
+
+    .name{
+        font-size: 22px;
     }
 
     img{
         max-width: 200px;
         max-height: 200px;
     }
+
+    .stats{
+        display: flex;
+        align-items: center;
+        span{
+            color: #374151;
+        }
+        strong{
+            color: #374151;
+        }
+
+        background-color: #fff;
+        width: 80px;
+    }
+
+    .bottom-info{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
 `
 
 interface PokemonCardProps {
@@ -43,40 +95,40 @@ export function PokemonCard(props: PokemonCardProps) {
             <div className="stats-box">
 
                 <div className="top-info">
-                    <div>
-                        <strong> Bulbasaur</strong>
-                        <div>
-                            <strong className="type">Grass</strong>
-                        </div>
+                    <strong className="name"> Bulbasaur</strong>
+                    <div className="container-type">
+                        <strong className="type">Grass</strong>
                     </div>
-                    <span>Seed Pokémon</span>
                 </div>
 
-                <div className="stats">
-                    <span>HP</span>
-                    <strong> 1071</strong>
-                </div>
+                <span>Seed Pokémon</span>
 
-                <div>
-                    <span>CP</span>
-                    <strong> 951</strong>
-                </div>
-                <div>
+                <div className="bottom-info">
+                    <div className="stats">
+                        <span>HP</span>
+                        <strong> 1071</strong>
+                    </div>
 
-                    <span>W</span>
-                    <strong> 0.79m</strong>
-                </div>
-                <div>
-                    <Shield size={32} weight="fill" />
-                    <strong> Water</strong>
-                </div>
-                <div>
-                    <Shield size={32} weight="fill" />
-                    <strong> Eletric</strong>
-                </div>
-                <div>
-                    <TrendDown size={32} weight="fill" />
-                    <strong> Fire</strong>
+                    <div className="stats">
+                        <span>CP</span>
+                        <strong> 951</strong>
+                    </div>
+                    <div className="stats">
+                        <span>W</span>
+                        <strong> 0.79m</strong>
+                    </div>
+                    <div className="stats">
+                        <Shield size={24} weight="fill" />
+                        <strong> Water</strong>
+                    </div>
+                    <div className="stats">
+                        <Shield size={24} weight="fill" />
+                        <strong> Eletric</strong>
+                    </div>
+                    <div className="stats">
+                        <TrendDown size={24} weight="fill" />
+                        <strong> Fire</strong>
+                    </div>
                 </div>
             </div>
         </Container>
